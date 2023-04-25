a254b516d6695126d740b6dec24ecb6267f6911a
DontLikenaming <edirna21@gmail.com>
2023-04-25 16:08:41 +0900

    sungjuklist 성적 조회

--- src/main/java/dontlikenaming/spring4/sungjukv6/controller/SungJukController.java
+++ src/main/java/dontlikenaming/spring4/sungjukv6/controller/SungJukController.java
@@ -5,8 +5,10 @@ import dontlikenaming.spring4.sungjukv6.service.SungJukV6Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SungJukController {
@@ -18,13 +20,23 @@ public class SungJukController {
         this.sjsrv = sjsrv;
     }
 
-    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    /*@RequestMapping(value = "/list", method = RequestMethod.GET)
     public String list(Model model) {
-        // System.out.println(sjsrv.readSungJuk());
 
         // sungjuklist.jsp에 성적 조회 결과 데이터를 넘김
         model.addAttribute("sjs",sjsrv.readSungJuk());
 
         return "sungjuklist";
+    }
+*/
+
+    @GetMapping(value = "/list")
+    public ModelAndView list() {
+
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("sjs", sjsrv.readSungJuk());
+        mav.setViewName("sungjuklist");
+
+        return mav;
     }
 }
